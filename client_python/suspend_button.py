616089596c0513b480b8352536b2323cfac1588c
Игорь Пугачев <wotwbb@gmail.com>
2020-04-18 17:30:15 +0300

    replace strings by constants

--- client_python/suspend_button.py
+++ client_python/suspend_button.py
@@ -3,10 +3,13 @@ from PyQt5.QtWidgets import QPushButton
 
 
 class SuspendButton(QPushButton):
+    SUSPEND_STATE = 'Приостановить'
+    RESUME_STATE = 'Продолжить'
+
     @pyqtSlot(name='switchState')
     def switch_state(self):
-        self.setText("Продолжить" if self.text() == "Приостановить" else "Приостановить")
+        self.setText(self.SUSPEND_STATE if self.text() == self.RESUME_STATE else self.RESUME_STATE)
 
     @pyqtSlot(name='setDefaultState')
     def set_default_state(self):
-        self.setText("Приостановить")
+        self.setText(self.SUSPEND_STATE)
